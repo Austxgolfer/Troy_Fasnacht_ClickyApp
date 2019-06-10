da5e970b0ec00296d75de1689f8864a3ba6f42f1
MAX <47061140+Austxgolfer@users.noreply.github.com>
2019-06-09 19:13:31 -0500

    cleared out unused filed from create-react. Added components ffolder with grid layout for app

---
+++ src/components/gridLayout.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./JSXVariables.css";
+var name = "Troy";
+var thoughts = "is interesting but confusing";
+
+function JSXVariables() {
+  return (
+  
+  
+
+export default JSXVariables;
