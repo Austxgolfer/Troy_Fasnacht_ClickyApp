09dcb2579ecbb6f24177d3d1952dc4cd32c044df
MAX <47061140+Austxgolfer@users.noreply.github.com>
2019-06-10 20:22:47 -0500

    added css grid to new index.css file. updated app.js to include divs for css grid. corrected all the function and file names so react can find all the files.  app currently displays test images and header but not in the corrrect layout

--- src/App.js
+++ src/App.js
@@ -1,8 +1,63 @@
 import React from "react";
-import gridLayout from "./components/gridLayout";
+import GridLayout from "./components/gridLayout";
 
-function App() {
-  return <gridLayout />;
+export default function App() {
+  return (
+    <section className="parent">
+      <div className="div1">
+        <ul>
+          <li>
+            <a className="active" href="#home">
+              Home
+            </a>
+          </li>
+          <li>
+            <a href="#news">News</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+          <li>
+            <a href="#about">About</a>
+          </li>
+        </ul>
+      </div>
+      <div className="div2">
+        <GridLayout />
+      </div>
+      <div className="div3">
+        <GridLayout />
+      </div>
+      <div className="div4">
+        <GridLayout />
+      </div>
+      <div className="div5">
+        <GridLayout />
+      </div>
+      <div className="div6">
+        <GridLayout />
+      </div>
+      <div className="div7">
+        <GridLayout />
+      </div>
+      <div className="div8">
+        <GridLayout />
+      </div>
+      <div className="div9">
+        <GridLayout />
+      </div>
+      <div className="div10">
+        <GridLayout />
+      </div>
+      <div className="div11">
+        <GridLayout />
+      </div>
+      <div className="div12">
+        <GridLayout />
+      </div>
+      <div className="div13">
+        <GridLayout />
+      </div>
+    </section>
+  );
 }
-
-export default App;
--- src/components/gridLayout.js
+++ src/components/gridLayout.js
@@ -1,11 +1,6 @@
 import React from "react";
-import "./JSXVariables.css";
-var name = "Troy";
-var thoughts = "is interesting but confusing";
+import "./gridLayout.css";
 
-function JSXVariables() {
-  return (
-  
-  
-
-export default JSXVariables;
+export default function GridLayout() {
+  return <img src={"../testimage.jpg"} alt="" />;
+}
