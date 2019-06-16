741f710e94deebe1b6a8264402720e6d7522ac88
MAX <47061140+Austxgolfer@users.noreply.github.com>
2019-06-16 18:35:10 -0500

    Updates

---
+++ precache-manifest.a2f2ff9069571005a405833fdf926d5c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f465192c1acfb0e60f48ed8bd9d1f504",
+    "url": "/Troy_Fasnacht_ClickyApp/index.html"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/Troy_Fasnacht_ClickyApp/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "fe8d8aa5e7cde6cbf229",
+    "url": "/Troy_Fasnacht_ClickyApp/static/js/main.f50378d0.chunk.js"
+  },
+  {
+    "revision": "7a4cb8f1b44b36509f5d",
+    "url": "/Troy_Fasnacht_ClickyApp/static/js/runtime~main.64ae6328.js"
+  }
+]);
