b3574337736f8789801b14e696a3eaaa8f6cf8a6
MAX <47061140+Austxgolfer@users.noreply.github.com>
2019-06-16 13:54:22 -0500

    extended gridlayout component to handle counter increment. also created pictures variable to handle images object.  click works and counter increments

--- src/components/gridLayout.js
+++ src/components/gridLayout.js
@@ -1,27 +1,45 @@
 import React from "react";
 
-export default function GridLayout(props) {
-  return (
-    <section className="container">
-      <div className="div1">
-        <ul>
-          <li className="inst">
-            <h2>
-              Click on and image. The board with shuffle than click another
-              image. See how long you can go with out clicky the same image
-            </h2>
-          </li>
-          <li className="score">
-            <h2>Current Successful Clicks/High Score</h2>
-          </li>
-        </ul>
-      </div>
-      {props.images.map(item => (
-        <span className="imageDiv">
-          <img className="image" key={item.id} src={item.link} />
-        </span>
-      ))}
-      ;
-    </section>
-  );
+class GridLayout extends React.Component {
+  state = {
+    count: 0
+  };
+
+  handleIncrement = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  render() {
+    const pictures = this.props.images;
+    return (
+      <section className="container">
+        <div className="div1">
+          <ul>
+            <li className="inst">
+              <h2>
+                Click on and image. The board with shuffle than click another
+                image. See how long you can go with out clicky the same image
+              </h2>
+            </li>
+            <li className="score">
+              <h2>Current Successful Clicks: {this.state.count} /High Score</h2>
+            </li>
+          </ul>
+        </div>
+        {pictures.map(pic => (
+          <span className="imageDiv">
+            <img
+              className="image"
+              key={pic.id}
+              src={pic.link}
+              onClick={this.handleIncrement}
+              alt=""
+            />
+          </span>
+        ))}
+        ;
+      </section>
+    );
+  }
 }
+export default GridLayout;
