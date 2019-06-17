8633f684ef44e70e0b7bfd17625e3c31f42d7f31
MAX <47061140+Austxgolfer@users.noreply.github.com>
2019-06-16 20:00:44 -0500

    trying another path

--- src/App.js
+++ src/App.js
@@ -5,51 +5,51 @@ import GridLayout from "./components/gridLayout";
 const Images = [
   {
     id: 0,
-    link: "../public/images/image0.jpg"
+    link: "/images/image0.jpg"
   },
   {
     id: 1,
-    link: "../public/images/image1.jpg"
+    link: "/images/image1.jpg"
   },
   {
     id: 2,
-    link: "../public/images/image2.jpg"
+    link: "/images/image2.jpg"
   },
   {
     id: 3,
-    link: "../public/images/image3.jpg"
+    link: "/images/image3.jpg"
   },
   {
     id: 4,
-    link: "../public/images/image4.jpg"
+    link: "/images/image4.jpg"
   },
   {
     id: 5,
-    link: "../public/images/image5.jpg"
+    link: "/images/image5.jpg"
   },
   {
     id: 6,
-    link: "../public/images/image6.jpg"
+    link: "/images/image6.jpg"
   },
   {
     id: 7,
-    link: "../public/images/image7.jpg"
+    link: "/images/image7.jpg"
   },
   {
     id: 8,
-    link: "../public/images/image8.jpg"
+    link: "/images/image8.jpg"
   },
   {
     id: 9,
-    link: "../public/images/image9.jpg"
+    link: "/images/image9.jpg"
   },
   {
     id: 10,
-    link: "../public/images/image10.jpg"
+    link: "/images/image10.jpg"
   },
   {
     id: 11,
-    link: "../public/images/image11.jpg"
+    link: "/images/image11.jpg"
   }
 ];
 
