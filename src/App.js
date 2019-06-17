f7f6fd557f7c1c9d7e69c4001e06c202acbb73c6
MAX <47061140+Austxgolfer@users.noreply.github.com>
2019-06-16 19:56:15 -0500

    gitpages not displaying images.  changing path to ../public/images/imagename

--- src/App.js
+++ src/App.js
@@ -5,51 +5,51 @@ import GridLayout from "./components/gridLayout";
 const Images = [
   {
     id: 0,
-    link: "../images/image0.jpg"
+    link: "../public/images/image0.jpg"
   },
   {
     id: 1,
-    link: "../images/image1.jpg"
+    link: "../public/images/image1.jpg"
   },
   {
     id: 2,
-    link: "../images/image2.jpg"
+    link: "../public/images/image2.jpg"
   },
   {
     id: 3,
-    link: "../images/image3.jpg"
+    link: "../public/images/image3.jpg"
   },
   {
     id: 4,
-    link: "../images/image4.jpg"
+    link: "../public/images/image4.jpg"
   },
   {
     id: 5,
-    link: "../images/image5.jpg"
+    link: "../public/images/image5.jpg"
   },
   {
     id: 6,
-    link: "../images/image6.jpg"
+    link: "../public/images/image6.jpg"
   },
   {
     id: 7,
-    link: "../images/image7.jpg"
+    link: "../public/images/image7.jpg"
   },
   {
     id: 8,
-    link: "../images/image8.jpg"
+    link: "../public/images/image8.jpg"
   },
   {
     id: 9,
-    link: "../images/image9.jpg"
+    link: "../public/images/image9.jpg"
   },
   {
     id: 10,
-    link: "../images/image10.jpg"
+    link: "../public/images/image10.jpg"
   },
   {
     id: 11,
-    link: "../images/image11.jpg"
+    link: "../public/images/image11.jpg"
   }
 ];
 
